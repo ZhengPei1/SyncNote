2a05bc6790a8ac7c16f37c273df7caf88b5be2b3
ZhengPei1 <zhengpei.pz@gmail.com>
2024-02-14 14:21:43 -0500

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <div id = "container">
+      <div id = "heading">h</div>
+      <div id = "editor">a</div>
+      <div id = "markdown">b</div>
+    </div>
+  )
+}
+
+export default App;
